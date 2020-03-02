ed02ffd6430acd59d7e134451888b4b340386b89
prakhar <prakhar.metacube@gmail.com>
2020-03-02 18:18:52 +0530

    EAD 8th assignment

---
+++ EAD8/src/main/java/com/spring/Ead7Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ead7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ead7Application.class, args);
+	}
+
+}
